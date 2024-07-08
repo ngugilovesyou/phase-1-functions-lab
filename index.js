a8443f8202fd8fc3729c9e1f42442d69b99ab605
Samuel <amsamgitau@gmail.com>
2024-07-08 20:52:07 +0300

    complete test

--- index.js
+++ index.js
@@ -15,27 +15,13 @@ function distanceFromHqInFeet (blockNumber) {
         return (start - destination) *264
     }
   }
-  /*function calculatesFarePrice(start, destination){
-    const distance = distanceTravelledInFeet(start, destination);
-    if(distance<=400){
-        return 0
-    }else if (distance>400 && distance>=2000){
-        return .02* (distance-400);
-    }else if (distance>2000) {
-        return 25; 
-    }else{
-        "cannot travel that far";
-    }
-
-  }*/
-
     function calculatesFarePrice (start, destination) {
         const distance = distanceTravelledInFeet(start, destination);
       
         if (distance <= 400) {
           return 0;
         } else if (distance > 400 && distance <= 2000) {
-          return .02 * (distance - 400);
+          return .02 * (distance - 400); //1dollar=100cents
         } else if (distance > 2000 && distance < 2500) {
           return 25;
         } else {
